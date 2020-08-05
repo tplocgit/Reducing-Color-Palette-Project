c2eee4efd2f946285e8262fb41a563eac69e7d50
Tran Phuoc Loc <giongnhuntlf@gmail.com>
2020-08-05 16:00:29 +0700

    DONE

--- SOURCE/18127130.py
+++ SOURCE/18127130.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import os
 
 INPUT_DIR = "..\\INPUT"
-OUTPUT_DIR = "..\\OUTPUT"
 NUM_CHANNELS = 3
 
 
@@ -23,10 +22,10 @@ def make_list_input_file(directory: str):
     return input_list
 
 
-def create_input(image):
+def create_input(img):
     """
     Reshape 2d of vectors to 1d of vectors
-    :param image: 
+    :param img:
     :return: 
     """
     img_mat = np.array(img)
@@ -94,17 +93,30 @@ def kmeans(img_1d, k_clusters, max_iter=1000, init_centroids=None):
     labels = group_data(img_1d, centroids)
     converged = False
     while max_iter > 0 and not converged:
+        # save old centroids to compare
         old_centroids = centroids
+        # evaluate new centroids
         centroids = update_centroids(img_1d, labels, k_clusters)
+        # group data and put into labels
         labels = group_data(img_1d, centroids)
+        # reduce max_iter to stop looping
         max_iter -= 1
         print("max_iter: ", max_iter)
+        # compare all element between old and current centroids
         converged = (old_centroids == centroids).all()
 
     return centroids, labels
 
 
 def reduce_color(img_1d, centroids, labels, size):
+    """
+    Reduce color ò img_1d by centroids and labels
+    :param img_1d:
+    :param centroids:
+    :param labels:
+    :param size:
+    :return:
+    """
     # set point to centroids
     for i in range(img_1d.shape[0]):
         img_1d[i] = centroids[int(labels[i])]
@@ -113,19 +125,27 @@ def reduce_color(img_1d, centroids, labels, size):
 
     return new_img
 
+
+def show_img_by_matrix(img_mat):
+    plt.imshow(img_mat)
+    plt.show()
+
+
 if __name__ == "__main__":
+    # INPUT MAKING
     print("Data analyzing...")
     list_items = make_list_input_file(INPUT_DIR)
-    print("Input images are: ", list_items)
-
-    with Image.open(list_items[1]) as img:
+    choices = [(i, e) for i, e in zip([i for i in range(len(list_items))], list_items)]
+    print("Input images are (index, file): ", choices)
+    chosen = int(input("Enter index of file you want to do stuff: "))
+    with Image.open(list_items[chosen]) as img:
         data_set = create_input(img)
+    # ENTER K_CLUSTERS
     k = int(input("Enter number of colors that you want to be performed: "))
+    # START REDUCING
     print("Reducing colors...")
     centroids, labels = kmeans(img_1d=data_set, k_clusters=k)
     new_img_mat = reduce_color(data_set, centroids, labels, img.size)
+    # DONE
     print('All done, Check it out')
-    new_img = Image.fromarray(new_img_mat)
-    new_img.save(OUTPUT_DIR,"")
-    plt.imshow(new_img_mat)
-    plt.show()
+    show_img_by_matrix(new_img_mat)
